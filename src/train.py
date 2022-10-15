46a7d7e58007bde9818be4f204755e8c6353c29e
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-10-15 16:04:28 +0200

    Replace decision tree with EBM

--- src/train.py
+++ src/train.py
@@ -3,42 +3,49 @@ import click
 import json
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
+from interpret.glassbox import ExplainableBoostingRegressor
 import joblib
 
 
-@click.command()
-@click.argument("input_path", type=str)
-@click.argument("output_path", type=str)
-def main(input_path, output_path):
-    feature_names = [
-        'TYPE_WACHTTIJD',
-        'SPECIALISME',
-        'ROAZ_REGIO',
-        'TYPE_ZORGINSTELLING'
-    ]
+FEATURE_NAMES = [
+    'TYPE_WACHTTIJD',
+    'SPECIALISME',
+    'ROAZ_REGIO',
+    'TYPE_ZORGINSTELLING'
+]
 
-    encoders = [(feature_name, OneHotEncoder())
-                for feature_name in feature_names]
+
+def get_model():
+    encoders = [(feature_name, OneHotEncoder(sparse=False))
+                for feature_name in FEATURE_NAMES]
 
     model_pipeline = Pipeline([
         ("combine_features", FeatureUnion(encoders)),
-        ("estimator", DecisionTreeRegressor())
+        ("estimator", ExplainableBoostingRegressor())
     ])
+
+    return model_pipeline
+
+
+@click.command()
+@click.argument("input_path", type=str, default='data/intermediate/wachttijden.csv')
+@click.argument("output_path", type=str, default='models/classifier.bin')
+def main(input_path, output_path):
+    model_pipeline = get_model()
 
     df = pd.read_csv(input_path)
     df_train, df_test = train_test_split(df, test_size=0.2)
 
-    df_train_features = df_train[feature_names]
-    df_test_features = df_test[feature_names]
+    df_train_features = df_train[FEATURE_NAMES]
+    df_test_features = df_test[FEATURE_NAMES]
 
     df_train_output = df_train['WACHTTIJD']
     df_test_output = df_test['WACHTTIJD']
 
     model_pipeline.fit(
-        df_train_features.to_numpy(),
-        df_train_output.to_numpy()
+        df_train_features,
+        df_train_output
     )
 
     score = model_pipeline.score(
@@ -50,6 +57,7 @@ def main(input_path, output_path):
         }))
 
     joblib.dump(model_pipeline, output_path)
+
 
 if __name__ == "__main__":
     main()
